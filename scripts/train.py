627e07f0fd538b1460dc6f1447306b1d6e10ac72
psj8252 <psj8252@naver.com>
2021-06-25 00:41:04 +0900

    feat: Add standard seq2seq train script

--- scripts/train.py
+++ scripts/train.py
@@ -1,98 +1,162 @@
 import argparse
-import json
 import sys
-from math import ceil
 
 import tensorflow as tf
+import tensorflow_text as text
+from transformers import BartConfig, TFBartForConditionalGeneration
 
-from transformers_bart_training.data import get_dataset
-from transformers_bart_training.model import SampleModel
-from transformers_bart_training.utils import LRScheduler, get_device_strategy, get_logger, path_join, set_random_seed
+from transformers_bart_training.data import (
+    filter_example,
+    get_dataset,
+    get_tfrecord_dataset,
+    make_train_examples,
+    slice_example,
+)
+from transformers_bart_training.measure import SparseCategoricalAccuracy, SparseCategoricalCrossentropy
+from transformers_bart_training.utils import (
+    LRScheduler,
+    get_device_strategy,
+    get_logger,
+    path_join,
+    set_mixed_precision,
+)
 
 # fmt: off
-parser = argparse.ArgumentParser()
-parser.add_argument("--model-config-path", type=str, required=True, help="model config file")
-parser.add_argument("--dataset-path", required=True, help="a text file or multiple files ex) *.txt")
-parser.add_argument("--pretrained-model-path", type=str, default=None, help="pretrained model checkpoint")
-parser.add_argument("--shuffle-buffer-size", type=int, default=5000)
-parser.add_argument("--output-path", default="output", help="output directory to save log and model checkpoints")
-parser.add_argument("--epochs", type=int, default=10)
-parser.add_argument("--steps-per-epoch", type=int, default=None)
-parser.add_argument("--learning-rate", type=float, default=2e-3)
-parser.add_argument("--min-learning-rate", type=float, default=1e-5)
-parser.add_argument("--warmup-rate", type=float, default=0.06)
-parser.add_argument("--warmup-steps", type=int)
-parser.add_argument("--batch-size", type=int, default=2)
-parser.add_argument("--dev-batch-size", type=int, default=2)
-parser.add_argument("--total-dataset-size", type=int, default=1000)
-parser.add_argument("--num-dev-dataset", type=int, default=2)
-parser.add_argument("--tensorboard-update-freq", type=int, default=1)
-parser.add_argument("--disable-mixed-precision", action="store_false", dest="mixed_precision", help="Use mixed precision FP16")
-parser.add_argument("--seed", type=int, help="Set random seed")
-parser.add_argument("--device", type=str, default="CPU", help="device to use (TPU or GPU or CPU)")
+parser = argparse.ArgumentParser("This is script to train seq2seq model")
+arg_group = parser.add_argument_group("File Paths")
+arg_group.add_argument("--model-config-path", type=str, required=True, help="model config file")
+arg_group.add_argument("--dataset-path", required=True, help="a text file or multiple files ex) *.txt")
+arg_group.add_argument("--pretrained-model-path", type=str, default=None, help="pretrained model checkpoint")
+arg_group.add_argument("--output-path", default="output", help="output directory to save log and model checkpoints")
+arg_group.add_argument("--sp-model-path", type=str, default="resources/sp-model/sp_model_unigram_16K.model")
+
+arg_group = parser.add_argument_group("Training Parameters")
+arg_group.add_argument("--epochs", type=int, default=10)
+arg_group.add_argument("--steps-per-epoch", type=int, default=None)
+arg_group.add_argument("--learning-rate", type=float, default=2e-4)
+arg_group.add_argument("--min-learning-rate", type=float, default=1e-5)
+arg_group.add_argument("--warmup-steps", type=int)
+arg_group.add_argument("--warmup-rate", type=float, default=0.06)
+arg_group.add_argument("--batch-size", type=int, default=512)
+arg_group.add_argument("--dev-batch-size", type=int, default=512)
+arg_group.add_argument("--num-total-dataset", type=int, default=1000000)
+arg_group.add_argument("--num-dev-dataset", type=int, default=30000)
+arg_group.add_argument("--shuffle-buffer-size", type=int, default=20000)
+arg_group.add_argument("--prefetch-buffer-size", type=int, default=1000)
+arg_group.add_argument("--max-sequence-length", type=int, default=256)
+
+arg_group = parser.add_argument_group("Other settings")
+arg_group.add_argument("--tensorboard-update-freq", type=int, help='log losses and metrics every after this value step')
+arg_group.add_argument("--mixed-precision", action="store_true", help="Use mixed precision FP16")
+arg_group.add_argument("--auto-encoding", action="store_true", help="train by auto encoding with text lines dataset")
+arg_group.add_argument("--use-tfrecord", action="store_true", help="train using tfrecord dataset")
+arg_group.add_argument("--debug-nan-loss", action="store_true", help="Trainin with this flag, print the number of Nan loss (not supported on TPU)")
+arg_group.add_argument("--device", type=str, default="CPU", choices= ["CPU", "GPU", "TPU"], help="device to train model")
+arg_group.add_argument("--max-over-sequence-policy", type=str, choices=["filter", "slice"], help="Policy for sequences of which length is over the max")
 # fmt: on
 
-if __name__ == "__main__":
-    args = parser.parse_args()
 
-    logger = get_logger()
+def main(args: argparse.Namespace):
+    strategy = get_device_strategy(args.device)
 
-    if args.seed:
-        set_random_seed(args.seed)
-        logger.info(f"Set random seed to {args.seed}")
+    logger = get_logger(__name__)
+
+    if args.mixed_precision:
+        set_mixed_precision(args.device)
+        logger.info("[+] Use Mixed Precision FP16")
 
     # Copy config file
     tf.io.gfile.makedirs(args.output_path)
     with tf.io.gfile.GFile(path_join(args.output_path, "argument_configs.txt"), "w") as fout:
         for k, v in vars(args).items():
             fout.write(f"{k}: {v}\n")
-    tf.io.gfile.copy(args.model_config_path, path_join(args.output_path, "model_config.json"))
+    tf.io.gfile.copy(args.model_config_path, path_join(args.output_path, "model_config.yml"))
 
-    with get_device_strategy(args.device).scope():
-        if args.mixed_precision:
-            mixed_type = "mixed_bfloat16" if args.device == "TPU" else "mixed_float16"
-            policy = tf.keras.mixed_precision.experimental.Policy(mixed_type)
-            tf.keras.mixed_precision.experimental.set_policy(policy)
-            logger.info("Use Mixed Precision FP16")
+    dataset_files = tf.io.gfile.glob(args.dataset_path)
+    if not dataset_files:
+        raise RuntimeError("Dataset path is invalid!")
 
-        # Construct Dataset
-        dataset_files = tf.io.gfile.glob(args.dataset_path)
-        if not dataset_files:
-            logger.error("[Error] Dataset path is invalid!")
-            sys.exit(1)
+    logger.info("[+] Load Tokenizer")
+    with tf.io.gfile.GFile(args.sp_model_path, "rb") as f:
+        tokenizer = text.SentencepieceTokenizer(f.read(), add_bos=True, add_eos=True)
 
-        dataset = get_dataset(dataset_files).shuffle(args.shuffle_buffer_size)
-        train_dataset = dataset.skip(args.num_dev_dataset).batch(args.batch_size)
-        dev_dataset = dataset.take(args.num_dev_dataset).batch(max(args.batch_size, args.dev_batch_size))
+    with strategy.scope():
+        logger.info("[+] Load Dataset")
+        if args.use_tfrecord:
+            dataset = get_tfrecord_dataset(dataset_files)
+        else:
+            dataset = get_dataset(dataset_files, tokenizer, args.auto_encoding)
+
+        # Apply policy for sequences whose length is over than max sequence length
+        if args.max_over_sequence_policy == "filter":
+            logger.info(f"[+] Filter examples whose sequence length is over than {args.max_sequence_length}")
+            dataset = dataset.filter(filter_example(args.max_sequence_length))
+        elif args.max_over_sequence_policy == "slice":
+            logger.info(f"[+] Slice examples whose sequence length is over than {args.max_sequence_length}")
+            dataset = dataset.map(
+                slice_example(args.max_sequence_length), num_parallel_calls=tf.data.experimental.AUTOTUNE
+            )
+        elif args.device == "TPU":
+            raise RuntimeError(f"You should set max-over-sequence-policy with TPU!")
+
+        # Make into Training Examples
+        dataset = dataset.shuffle(args.shuffle_buffer_size).map(
+            make_train_examples, num_parallel_calls=tf.data.experimental.AUTOTUNE
+        )
+        train_dataset = dataset.skip(args.num_dev_dataset)
+        dev_dataset = dataset.take(args.num_dev_dataset)
 
         if args.steps_per_epoch:
+            logger.info("[+] Repeat dataset")
             train_dataset = train_dataset.repeat()
-            logger.info("Repeat dataset")
 
-        # Model Initialize
-        with tf.io.gfile.GFile(args.model_config_path) as f:
-            model = SampleModel(**json.load(f))
+        logger.info("[+] Initialize Model")
+        model_config = BartConfig.from_pretrained(args.model_config_path)
+        model = TFBartForConditionalGeneration(model_config)
 
-        # Load pretrained model
-        if args.pretrained_model_path:
-            model.load_weights(args.pretrained_model_path)
-            logger.info("Loaded weights of model")
-
-        # Model Compile
-        total_steps = ceil((args.total_dataset_size - args.num_dev_dataset) / args.batch_size) * args.epochs
-        model.compile(
-            optimizer=tf.optimizers.Adam(
-                LRScheduler(
-                    total_steps, args.learning_rate, args.min_learning_rate, args.warmup_rate, args.warmup_steps
-                )
-            ),
-            loss=tf.keras.losses.BinaryCrossentropy(),
-            metrics=[tf.keras.metrics.BinaryAccuracy()],
+        # Batching
+        pad_length = None if args.device != "TPU" else args.max_sequence_length
+        pad_shape = (
+            {"input_ids": [pad_length], "decoder_input_ids": [pad_length - 1 if pad_length else None]},
+            [pad_length - 1 if pad_length else None],
         )
-        logger.info("Model compiling complete")
-        logger.info("Start training")
+        pad_values = (
+            {"input_ids": model_config.pad_token_id, "decoder_input_ids": model_config.pad_token_id},
+            model_config.pad_token_id,
+        )
+        train_dataset = train_dataset.padded_batch(args.batch_size, pad_shape, pad_values).prefetch(
+            args.prefetch_buffer_size
+        )
+        dev_dataset = dev_dataset.padded_batch(args.dev_batch_size, pad_shape, pad_values)
 
-        # Training
+        model(
+            {
+                "input_ids": tf.keras.Input([pad_length], dtype=tf.int32),
+                "decoder_input_ids": tf.keras.Input([pad_length - 1 if pad_length else None], dtype=tf.int32),
+            }
+        )
+        model.summary()
+
+        if args.pretrained_model_path:
+            logger.info("[+] Load weights of trained model")
+            model.load_weights(args.pretrained_model_path)
+
+        logger.info("[+] Compile Model")
+        total_steps = (args.num_total_dataset - args.num_dev_dataset) // args.batch_size  # TODO: Device number
+        learning_rate = LRScheduler(
+            total_steps, args.learning_rate, args.min_learning_rate, args.warmup_rate, args.warmup_steps
+        )
+
+        model.compile(
+            optimizer=tf.optimizers.Adam(learning_rate),
+            loss={
+                "logits": SparseCategoricalCrossentropy(model_config.pad_token_id, from_logits=True),
+                "encoder_last_hidden_state": None,
+            },
+            metrics={"logits": SparseCategoricalAccuracy(model_config.pad_token_id)},
+        )
+
+        logger.info("[+] Start training")
         model.fit(
             train_dataset,
             validation_data=dev_dataset,
@@ -103,13 +167,19 @@ if __name__ == "__main__":
                     path_join(
                         args.output_path,
                         "models",
-                        "model-{epoch}epoch-{val_loss:.4f}loss_{val_binary_accuracy:.4f}acc.ckpt",
+                        "model-{epoch}epoch-{val_loss:.4f}loss_{val_logits_accuracy:.4f}acc.ckpt",
                     ),
                     save_weights_only=True,
                     verbose=1,
                 ),
                 tf.keras.callbacks.TensorBoard(
-                    path_join(args.output_path, "logs"), update_freq=args.tensorboard_update_freq
+                    path_join(args.output_path, "logs"),
+                    update_freq=args.tensorboard_update_freq if args.tensorboard_update_freq else "batch",
                 ),
             ],
         )
+        logger.info("[+] Finished training!")
+
+
+if __name__ == "__main__":
+    sys.exit(main(parser.parse_args()))
