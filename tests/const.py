7462e4a4cf65f749e2559a2fd16278981b90d25a
psj8252 <psj8252@naver.com>
2021-07-03 00:59:11 +0900

    feat: Add sp models

--- tests/const.py
+++ tests/const.py
@@ -2,4 +2,4 @@ import os
 
 TEST_DIR = os.path.abspath(os.path.dirname(__file__))
 TEST_DATA_DIR = os.path.join(TEST_DIR, "data")
-DEFAULT_SPM_MODEL = os.path.join(TEST_DIR, "..", "sp_model", "sp_model_unigram_8K.model")
+DEFAULT_SPM_MODEL = os.path.join(TEST_DIR, "data", "test_sp_model_8K.model")
